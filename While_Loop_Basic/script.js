b8c9d7db87ee1da49ee76c5fc89db01a9105204f
Adam_Codes <76964302+Adam0290@users.noreply.github.com>
2021-04-21 16:55:16 +0100

    Add files via upload

---
+++ While_Loop_Basic/script.js
@@ -0,0 +1,12 @@
+// While Loops // 
+
+
+let arr = ["milk", "bread", "cheese", "butter",];
+let count = 0; // This code counts how many times this loop will repeat //
+while(count < arr.length){
+console.log(arr[count++]);
+}
+
+// count++ >>>> This code stops it from becoming a forever loop >>>> And prints the array in the correct order // 
+
+// We put things inside loops so that they can be executed repeatedly //
